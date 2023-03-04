1c650f4babc20ffa94e8add956db5471d6f0a117
Cleberton Kobay <Clebertonkobay@gmail.com>
2023-03-04 17:25:37 -0300

    adição de animações

--- mobile/src/components/CheckBox.tsx
+++ mobile/src/components/CheckBox.tsx
@@ -1,6 +1,7 @@
 import { View, TouchableOpacity, TouchableOpacityProps, Text } from "react-native";
 import { Feather } from '@expo/vector-icons'
 import colors from 'tailwindcss/colors'
+import Animated, { ZoomIn, ZoomOut } from "react-native-reanimated"
 
 interface Props extends TouchableOpacityProps{
     title: string
@@ -12,20 +13,22 @@ export function CheckBox({ title,checked = false, ...rest }:Props){
 
         <TouchableOpacity
             activeOpacity={0.7}
-            className="flex-row mb-2 items-center"
+            className="flex-row mb-2 items-center" 
             {...rest}
         >
             {checked 
                 ?
-                <View
+                <Animated.View
                     className="h-8 w-8 bg-green-800 rounded-lg items-center justify-center "
+                    entering={ZoomIn}
+                    exiting={ZoomOut}
                 >
                     <Feather
                         name="check"
                         size={20}
                         color={colors.white}
                     />
-                </View>
+                </Animated.View>
                 :
                 <View
                     className="h-8 w-8 bg-zinc-900 rounded-lg"
